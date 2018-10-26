15df1c5ebc2b14d51bba4b214ca23bc6537dff35
Raghav501 <raghavendra9ygmail.com>
2018-10-26 09:04:53 +0530

    Adding Train class commit
    Signed-off-by: SeleTrail <seletrails09@gmail.com>

---
+++ HealthCare/src/main/java/HealthCare/App_Train.java
@@ -0,0 +1,13 @@
+package HealthCare;
+
+/**
+ * Hello world!
+ *
+ */
+public class App_Train 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
